809e05821c584c663463801d5943ca8a771e376f
seintun <seintun.edu@gmail.com>
2019-04-03 20:07:13 -0700

    Added increment handler for Up/Down vote

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -8,6 +8,40 @@ export default class Vote extends Component {
     dislike: 25,
     status: null
   };
+  incrementLike = () => {
+    let likeCount = this.state.like;
+    let dislikeCount = this.state.dislike;
+
+    if (this.state.status === null) {
+      this.setState({
+        status: "like",
+        like: likeCount + 1
+      });
+    } else if (this.state.status === "dislike") {
+      this.setState({
+        status: "like",
+        like: likeCount + 1,
+        dislike: dislikeCount - 1
+      });
+    }
+  };
+  incrementDislike = () => {
+    let likeCount = this.state.like;
+    let dislikeCount = this.state.dislike;
+
+    if (this.state.status === null) {
+      this.setState({
+        status: "dislike",
+        dislike: dislikeCount + 1
+      });
+    } else if (this.state.status === "like") {
+      this.setState({
+        status: "dislike",
+        dislike: dislikeCount + 1,
+        like: likeCount - 1
+      });
+    }
+  };
   render() {
     const PLACEHOLDER_IMAGE =
       "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg";
@@ -21,11 +55,11 @@ export default class Vote extends Component {
             alt={`The movie titled: Avengers: Endgame`}
           />
         </div>
-        <button>
+        <button onClick={this.incrementLike}>
           <FontAwesomeIcon icon={faThumbsUp} /> |{" "}
           <span className="likes-counter">{this.state.like}</span>
         </button>
-        <button>
+        <button onClick={this.incrementDislike}>
           <FontAwesomeIcon icon={faThumbsDown} /> |{" "}
           <span className="dislikes-counter">{this.state.dislike}</span>
         </button>
