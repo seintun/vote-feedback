1416e05d0479cb18fcf831a41d105408cc1000b8
seintun <seintun.edu@gmail.com>
2019-04-03 20:05:40 -0700

    Installed FontAwesome for button, Added state for counter

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 
 export default class Vote extends Component {
+  state = {
+    like: 100,
+    dislike: 25,
+    status: null
+  };
   render() {
+    const PLACEHOLDER_IMAGE =
+      "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg";
     return (
       <div className="post">
-        <button>Like</button>
-        <button>Dislike</button>
+        <div>
+          <p>Are you watching, Avengers: Endgame?</p>
+          <img
+            width="200"
+            src={PLACEHOLDER_IMAGE}
+            alt={`The movie titled: Avengers: Endgame`}
+          />
+        </div>
+        <button>
+          <FontAwesomeIcon icon={faThumbsUp} /> |{" "}
+          <span className="likes-counter">{this.state.like}</span>
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faThumbsDown} /> |{" "}
+          <span className="dislikes-counter">{this.state.dislike}</span>
+        </button>
       </div>
     );
   }
